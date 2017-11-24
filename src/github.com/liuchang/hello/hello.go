0802ae22114571a9c8e9dcd0c6ec6ba7b5d534c8
Liu Chang <liuchang@qiniu.com>
2017-11-24 11:46:16 +0800

    add stringutil package

--- src/github.com/liuchang/hello/hello.go
+++ src/github.com/liuchang/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/liuchang/stringutil"
+)
 
 func main() {
 	fmt.Printf("Hello World.\n")
+	fmt.Printf(stringutil.Reverse("Hello World.") + "\n")
 }
